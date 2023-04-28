a34f0009772698258572a4b5ccab017defd85271
nima.afzal.ahangaran@gmail.com <nima.afzal.ahangaran@gmail.com>
2023-04-29 02:48:40 +0330

    fix: change the code and apply the required fixes to pass all the tests

--- src/main/java/com/ncr/test/pyramid/solver/impl/NaivePyramidSolver.java
+++ src/main/java/com/ncr/test/pyramid/solver/impl/NaivePyramidSolver.java
@@ -9,11 +9,20 @@ import com.ncr.test.pyramid.solver.PyramidSolver;
 public class NaivePyramidSolver implements PyramidSolver {
     @Override
     public long pyramidMaximumTotal(Pyramid pyramid) {
+        // Fixed handling NULL parameter
+        if(pyramid == null){
+            throw new IllegalArgumentException();
+        }
         return getTotalAbove(pyramid.getRows() - 1, 0, pyramid);
     }
 
     private long getTotalAbove(int row, int column, Pyramid pyramid) {
-        if (row == 0) return 0;
+        // Fixed bug - adding check for empty pyramids
+        if(row < 0) return 0;
+
+        // Fixed bug - should return the elements of the topmost row
+        // Returning zero will omit the values of the topmost elements that is the reason for returning wrong result
+        if (row == 0) return pyramid.get(row,column);
 
         int myValue = pyramid.get(row, column);
         long left  = myValue + getTotalAbove(row - 1, column, pyramid);
