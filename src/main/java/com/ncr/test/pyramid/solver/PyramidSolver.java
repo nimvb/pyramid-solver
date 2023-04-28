7107584f78aea8cfa7a214076b3fe3b6795a1bd9
nima.afzal.ahangaran@gmail.com <nima.afzal.ahangaran@gmail.com>
2023-04-29 02:13:52 +0330

    docs: add documentation for pyramid solver method

--- src/main/java/com/ncr/test/pyramid/solver/PyramidSolver.java
+++ src/main/java/com/ncr/test/pyramid/solver/PyramidSolver.java
@@ -7,5 +7,11 @@ import com.ncr.test.pyramid.data.Pyramid;
  *  Refer to {@link com.ncr.test.pyramid.data.Pyramid} for problem description.
  */
 public interface PyramidSolver {
+    /**
+     * Calculate the maximum sum for the given inverted pyramid elements(bottom-to-top)
+     * @param pyramid the pyramid object
+     * @throws IllegalArgumentException if the given {@code pyramid} is {@code null}
+     * @return maximum sum of the inverted pyramid elements
+     */
     long pyramidMaximumTotal(Pyramid pyramid);
 }
