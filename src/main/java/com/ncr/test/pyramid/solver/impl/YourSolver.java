3c2a349fa1af7da346f7d05f3cf229814e1fda45
Nima Afzal <nima.afzal.ahangaran@gmail.com>
2023-04-29 20:52:53 +0330

    feat: add enhanced version of pyramid solver (#11)
    * feat(test): add additional tests for validating the implementation of the enhanced pyramid solver
    
    * feat: implement the enhanced version o the pyramid solver
    
    The implementation of the pyramid solver is enhanced via using Dynamic Programming technique. Each cell value is going to be calculated once based on this formula:
    value(i,j) = max(value(i-1,j) + value(i,j) ,value(i-1,j+1) + value(i,j))
    
    * feat: increase the size of large data
    
    refactor(gradle): change the maximum heap size the program can allocate for running tests
    
    Using Dynamic Programming enhances the execution time for the pyramid solver. However, this approach requires much more memory. Therefore, it is necessary to increase the amount of memory the application can use.

--- src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
+++ src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
@@ -10,8 +10,48 @@ import com.ncr.test.pyramid.solver.PyramidSolver;
 public class YourSolver implements PyramidSolver {
 
     @Override
-    public long pyramidMaximumTotal(Pyramid pyramid) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public long pyramidMaximumTotal(Pyramid pyr) {
+
+        Pyramid pyramid = validate(pyr);
+
+        // Return zero for empty pyramids
+        if(pyramid.getRows() == 0){
+            return 0;
+        }
+
+        int      rowCount = pyramid.getRows();
+        long[][] table   = new long[rowCount][rowCount];
+
+        // Fill the first rows of the table with the values of the topmost row of the pyramid
+        for (int i = 0 ; i < rowCount;i ++){
+            table[0][i] = pyramid.get(0,i);
+        }
+
+        // Fill the remaining rows with the rule:
+        // table(i,j) = max(table(i-1,j) + table(i,j) ,table(i-1,j+1) + table(i,j))
+        for (int i = 1; i < rowCount; i++){
+            for (int  j = 0 ; j<rowCount - i ;j++){
+                long left = table[i-1][j] + pyramid.get(i,j);
+                long right = table[i-1][j + 1] + pyramid.get(i,j);
+                table[i][j] = Math.max(left,right);
+            }
+        }
+
+        // The head of the inverted pyramid stores the maximum sum
+        return table[rowCount - 1][0];
+    }
+
+    /**
+     * Validates the given {@code pyramid} and re
+     * @param pyramid the pyramid object
+     * @throws IllegalArgumentException if the {@code pyramid} is {@code null}
+     * @return the {@code pyramid} object itself
+     */
+    private Pyramid validate(Pyramid pyramid) throws IllegalArgumentException{
+        if(pyramid == null){
+            throw new IllegalArgumentException();
+        }
+        return pyramid;
     }
     
 }
--- src/test/java/com/ncr/test/pyramid/solver/YourSolverTest.java
+++ src/test/java/com/ncr/test/pyramid/solver/YourSolverTest.java
@@ -7,11 +7,10 @@ import com.ncr.test.pyramid.solver.impl.YourSolver;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class YourSolverTest {
-    private static final int MAX_DEPTH = 100;
+    private static final int MAX_DEPTH = 10000;
 
     private static final int[][] SAMPLE_DATA = {
             { 5, 9, 8, 4 },
@@ -24,6 +23,27 @@ public class YourSolverTest {
             { 87,   1, 70,  0 },
             { 36,  41,  0,  0 },
             { 23,   0,  0,  0 }
+    };
+
+    private static final int[][] NEGATIVE_DATA = {
+            {-7, -8, -3, -10},
+            {-4, -5, -6, 0},
+            {-2, -3, 0, 0},
+            {-1, 0, 0, 0}
+    };
+
+    private static final int[][] MIXED_NEGATIVE_POSITIVE_DATA = {
+            {-7, -8, -3, 10},
+            {-4, -5, -9, 0},
+            {-2, -3, 0, 0},
+            {-1, 0, 0, 0}
+    };
+
+    private static final int[][] DEMO_DATA_WITH_NON_ZERO_NEUTRAL_VALUES   = {
+            {59, 207, 98, 95},
+            {87, 1, 70, -80},
+            {36, 41, 0, 1000},
+            {23, 13, 12, 500}
     };
 
     protected PyramidSolver solver;
@@ -59,5 +79,47 @@ public class YourSolverTest {
         final Pyramid pyramid = generator.generatePyramid();
 
         assertEquals("Max path in 'random' pyramid", 398, this.solver.pyramidMaximumTotal(pyramid));
+    }
+
+    @Test
+    public void givenEmptyPyramid_shouldReturnZero() {
+        final Pyramid pyramid = new Pyramid(new int[0][]);
+
+        assertEquals("Max path in 'empty' pyramid", 0, this.solver.pyramidMaximumTotal(pyramid));
+    }
+
+    @Test
+    public void givenPyramidWithSingleElement_shouldReturnTheElementValue() {
+        int[][] data = new int[1][];
+        data[0] = new int[]{50};
+
+        final Pyramid singleElementPyramid = new Pyramid(data);
+
+        assertEquals("Max path in 'single-element' pyramid", 50, this.solver.pyramidMaximumTotal(singleElementPyramid));
+    }
+
+    @Test
+    public void givenNULLPyramid_shouldThrownAnException() {
+        assertThrows(IllegalArgumentException.class, () -> this.solver.pyramidMaximumTotal(null));
+    }
+
+    @Test
+    public void givenPyramidWithNegativeValues_shouldReturnCorrectMaximumValue() {
+        final Pyramid pyramid = new Pyramid(NEGATIVE_DATA);
+
+        assertEquals("Max path in 'all-negative' pyramid", -11, this.solver.pyramidMaximumTotal(pyramid));
+    }
+
+    @Test
+    public void givenPyramidWithMixedValues_shouldReturnCorrectMaximumValue() {
+        final Pyramid pyramid = new Pyramid(MIXED_NEGATIVE_POSITIVE_DATA);
+
+        assertEquals("Max path in 'mix-negative-positive' pyramid", -3, this.solver.pyramidMaximumTotal(pyramid));
+    }
+
+    @Test
+    public void givenDemoDataWithNonZeroNeutralValues_shouldReturnTheCorrectResult() {
+        Pyramid pyramid = new Pyramid(DEMO_DATA_WITH_NON_ZERO_NEUTRAL_VALUES);
+        assertEquals("Max path in Demo pyramid", 353, solver.pyramidMaximumTotal(pyramid));
     }
 }
